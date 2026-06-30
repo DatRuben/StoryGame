42245faf58cad7eec0f775250e174d4afcc1a22e
RubenB232 <rubenbecerril.rb@gmail.com>
2026-06-29 22:50:34 -0500

    Added DamageReceiver

--- Assets/Scripts/Player/PlayerResources.cs
+++ Assets/Scripts/Player/PlayerResources.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class PlayerResources : MonoBehaviour
+public class PlayerResources : MonoBehaviour, DamageReceiver
 {
     [Header("Health")]
     private float maxHealth;
