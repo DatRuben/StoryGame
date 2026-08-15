acc52da56a8ae50be1424ef09772c7f1d89d3eb7
RubenB232 <rubenbecerril.rb@gmail.com>
2026-08-15 15:36:23 -0500

    created a world item spawner in Game Systems

---
+++ Assets/Scripts/Inventories/WorldItemSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public sealed class WorldItemSpawner :
+    MonoBehaviour
+{
+    [SerializeField]
+    private WorldItem worldItemPrefab;
+
+    public bool TrySpawn(
+        InventoryItemInstance item,
+        Vector3 position,
+        Quaternion rotation,
+        out WorldItem worldItem)
+    {
+        worldItem = null;
+
+        if (worldItemPrefab == null ||
+            item == null ||
+            item.IsEmpty ||
+            item.Definition == null ||
+            item.Definition.worldPrefab == null)
+        {
+            return false;
+        }
+
+        worldItem =
+            Instantiate(
+                worldItemPrefab,
+                position,
+                rotation
+            );
+
+        if (worldItem == null)
+            return false;
+
+        if (worldItem.Initialize(item))
+            return true;
+
+        Destroy(worldItem.gameObject);
+        worldItem = null;
+
+        return false;
+    }
+}
